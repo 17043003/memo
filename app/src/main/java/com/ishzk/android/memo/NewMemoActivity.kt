1550768a7da4f93e40ce4dd8f5f1c4e40b332a0e
17043003 <ishizuka533725@gmail.com>
2022-05-22 23:05:15 +0900

    show new memo page at add memo button is clicked.

---
+++ app/src/main/java/com/ishzk/android/memo/NewMemoActivity.kt
@@ -0,0 +1,11 @@
+package com.ishzk.android.memo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewMemoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_memo)
+    }
+}
