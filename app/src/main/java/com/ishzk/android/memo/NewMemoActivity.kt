eb6ebc09622a121d0af1fff0098a85da67b1bb6a
17043003 <ishizuka533725@gmail.com>
2022-05-24 02:14:16 +0900

    add save button to menu.

--- app/src/main/java/com/ishzk/android/memo/NewMemoActivity.kt
+++ app/src/main/java/com/ishzk/android/memo/NewMemoActivity.kt
@@ -2,10 +2,16 @@ package com.ishzk.android.memo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class NewMemoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_memo)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_new_memo, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
