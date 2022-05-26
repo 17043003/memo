dd0e159919c0a9e923e8ccf4c1546706ab355709
17043003 <ishizuka533725@gmail.com>
2022-05-27 02:10:32 +0900

    add data info to memo model.

--- app/src/main/java/com/ishzk/android/memo/Model/Memo.kt
+++ app/src/main/java/com/ishzk/android/memo/Model/Memo.kt
@@ -1,6 +1,10 @@
 package com.ishzk.android.memo.Model
 
+import java.util.*
+
 data class Memo(
     val title: String,
     val content: String,
+    val createdAt: Calendar,
+    val updatedAt: Calendar,
 )
