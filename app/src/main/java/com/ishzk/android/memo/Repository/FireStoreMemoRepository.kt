0a1f341e20ad18f4c4c59645e85ff15216e7225f
17043003 <ishizuka533725@gmail.com>
2022-06-05 03:59:20 +0900

    add fetch memo query.

--- app/src/main/java/com/ishzk/android/memo/Repository/FireStoreMemoRepository.kt
+++ app/src/main/java/com/ishzk/android/memo/Repository/FireStoreMemoRepository.kt
@@ -1,10 +1,17 @@
 package com.ishzk.android.memo.Repository
 
-import java.util.Calendar
 import android.util.Log
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.ishzk.android.memo.Model.Memo
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
+import java.util.*
+import kotlin.coroutines.suspendCoroutine
 
 private const val TAG = "FireStoreMemoRepository"
 
@@ -19,5 +26,12 @@ class FireStoreMemoRepository: MemoRepository {
             .addOnFailureListener { e ->
                 Log.w(TAG, "Error adding memo.", e)
             }
+    }
+
+    override fun fetchMemos(): Query {
+        val db = Firebase.firestore
+        return db.collection("memos")
+            .orderBy("updatedAt", Query.Direction.ASCENDING)
+            .limit(50)
     }
 }
--- app/src/main/java/com/ishzk/android/memo/Repository/MemoRepository.kt
+++ app/src/main/java/com/ishzk/android/memo/Repository/MemoRepository.kt
@@ -1,7 +1,9 @@
 package com.ishzk.android.memo.Repository
 
+import com.google.firebase.firestore.Query
 import com.ishzk.android.memo.Model.Memo
 
 interface MemoRepository {
     fun saveMemo(memo: Memo)
+    fun fetchMemos(): Query
 }
